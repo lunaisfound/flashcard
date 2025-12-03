8331889bbb47810e8fe64bbd1d002f0160e9d0e5
lunaisfound <130017998+lunaisfound@users.noreply.github.com>
2025-12-03 00:55:52 -0600

    feat: add form to create/edit card

---
+++ client/src/Form.jsx
@@ -0,0 +1,34 @@
+export default function Form({
+  front,
+  back,
+  setFront,
+  setBack,
+  onSubmit,
+  editing,
+}) {
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type="text"
+        placeholder="Front text"
+        value={front}
+        onChange={(e) => setFront(e.target.value)}
+      />
+      <br />
+      <br />
+
+      <input
+        type="text"
+        placeholder="Back text"
+        value={back}
+        onChange={(e) => setBack(e.target.value)}
+      />
+      <br />
+      <br />
+
+      <button type="submit">
+        {editing ? "Save Changes" : "Add Flashcard"}
+      </button>
+    </form>
+  );
+}
