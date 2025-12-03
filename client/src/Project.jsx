0c3b749e49aaa7df1ecd22af5c9e0cfd0e0893cd
lunaisfound <130017998+lunaisfound@users.noreply.github.com>
2025-12-03 00:09:53 -0600

    feat: add Project display that shows all decks in 1 project

--- client/src/Project.jsx
+++ client/src/Project.jsx
@@ -1,10 +1,24 @@
 import { Flashcard } from "react-quizlet-flashcard";
 import "react-quizlet-flashcard/dist/index.css";
-const MyFlashcard = () => (
-  <Flashcard
-    front={{ html: <div>Front Content</div> }}
-    back={{ html: <div>Back Content</div> }}
-  />
-);
+import { useEffect, useState } from "react";
 
-export default MyFlashcard;
+export default function Project() {
+  let deckList = [];
+  useEffect(() => {
+    fetch("http://localhost:5000/...") //im not sure about the location
+      .then((res) => res.json())
+      .then((data) => {
+        deckList.push(data);
+      });
+  }, [deckList]);
+
+  return (
+    <ul>
+      {deckList.map((item, index) => (
+        <li key={index}>
+          <link href="...">{item}</link>
+        </li>
+      ))}
+    </ul>
+  );
+}
